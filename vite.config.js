d114b34d78fff36f8e289ca60a95a880db022a01
Priya dharshini <88586314+priyahitee@users.noreply.github.com>
2024-03-22 19:07:57 +0530

    first commit - file upload

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Set a higher limit (in kB) to avoid the warning
+  },
+})
+
+
